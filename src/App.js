b159f87d971295dcb53174254151817e63e924eb
Felipe Gorgo Kiçula <felipegorgo25@gmail.com>
2024-08-12 17:30:58 -0300

    Pokedex01

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
+  const [pokemonList, setPokemonList] = useState([]);
+
+  useEffect(() => {
+    const fetchAllPokemon = async () => {
+      const url = 'https://pokeapi.co/api/v2/pokemon?limit=1025';
+      const response = await fetch(url);
+      const data = await response.json();
+
+      const pokemonDetailsPromises = data.results.map(async (pokemon) => {
+        const res = await fetch(pokemon.url);
+        return res.json();
+      });
+
+      const allPokemonDetails = await Promise.all(pokemonDetailsPromises);
+      setPokemonList(allPokemonDetails);
+    };
+
+    fetchAllPokemon();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header className="pokedex">
+        <h1>Pokedex</h1>
       </header>
+
+      {pokemonList.map((pokemon, index) => (
+        <article key={index} className="post">
+          <strong>{pokemon.name}</strong>
+          <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+          <p>Altura: {pokemon.height} decímetros</p>
+          <p>Peso: {pokemon.weight} hectogramas</p>
+          <p>Experiência básica: {pokemon.base_experience}</p>
+        </article>
+      ))}
     </div>
   );
 }
